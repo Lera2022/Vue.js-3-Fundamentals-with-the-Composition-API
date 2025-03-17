59240296f57db3eddd9e47761e7b1938a9300633
Lera <valeriiakolesnikova21@gmail.com>
2025-03-17 18:44:07 +0500

    lesson 1

---
+++ App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from 'vue';
+
+
+const msg = ref('Hello World!');
+</script>
+
+<template>
+  <!--  <h1>{{ msg }}</h1>-->
+  <h1>{{ msg.toLocaleUpperCase() }}</h1>
+  <input v-model='msg'>
+  <!--  <input v-model="msg">-->
+</template>
